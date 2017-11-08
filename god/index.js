3f05d51138111e65fdc305c9ac7a7c485259f76f
Agney <agney@dexlock.com>
2017-11-08 11:40:10 +0530

    Read and write cookies

--- god/index.js
+++ god/index.js
@@ -6,8 +6,23 @@ app.use(cookieParser());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(function (req, res, next) {
+  var cookie = req.cookies.godCookie;
+  if (cookie === undefined) {
+    var randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    res.cookie('godCookie', randomNumber, { maxAge: 60 * 60 * 24 * 7 });
+    console.log('Cookie created successfully');
+  }
+  else {
+    console.log('Cookie exists', cookie);
+  }
   next();
 });
 
