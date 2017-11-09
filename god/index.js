9bf49ba868e502579f6800c444cd61dafc67c7f2
Agney <agney@dexlock.com>
2017-11-09 11:04:15 +0530

    Cookies js error corrected

--- god/index.js
+++ god/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cookieParser());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use(function (req, res, next) {
   var cookie = req.cookies.godCookie;
